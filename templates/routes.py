540aa8c46e945b9153c16eaf2e251d6c8668d15c
Sofia-Oles <55633449+Sofia-Oles@users.noreply.github.com>
2020-12-05 01:18:57 +0200

    Initial commit

--- templates/routes.py
+++ templates/routes.py
@@ -1,8 +1,8 @@
 from flask import request, redirect, flash, render_template
 from flask_login import login_user
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from app import app
-
+from Database import db
 from Models.ModelUser import ModelUser
 
 # LOGIN
@@ -45,9 +45,28 @@ def Login():
 
 
 # REGISTER
+# link to try: http://127.0.0.1:5000/register
 # назву методу лишаєте таку ж, в нас на вас ссилочка)) решту дописуйте
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def Register():
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash('Please, fill all fields!')
+        elif password != password2:
+            flash('Passwords are not equal!')
+        else:
+            hash_pwd = generate_password_hash(password)
+            new_user = ModelUser(user_login=login, user_password=hash_pwd)
+            db.session.add(new_user)
+            db.session.commit()
+
+            # return redirect(url_for('login.html'))
+
     return render_template('register.html')
